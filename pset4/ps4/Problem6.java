04f5c23e7ce32845f18e530ca1feeea4299a7a96
Dave K <davidpkalish@gmail.com>
2018-04-16 17:18:57 -0400

    half of problem 6 done

--- pset4/ps4/Problem6.java
+++ pset4/ps4/Problem6.java
@@ -9,8 +9,8 @@ import java.util.*;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 
-/* 
- * interfaces and classes for Hadoop data types that you may 
+/*
+ * interfaces and classes for Hadoop data types that you may
  * need for some or all of the problems from PS 4
  */
 import org.apache.hadoop.io.ArrayWritable;
@@ -32,31 +32,136 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class Problem6 {
 
-    /* 
-     * Put your mapper and reducer classes here. 
-     * Remember that they should be static nested classes. 
+    /*
+     * This mapper maps each input line to a set of (word, 1) pairs,
+     * with one pair for each word in the line.
      */
+    public static class MyMapper1 extends
+        Mapper<Object, Text, IntWritable, IntWritable>
+    {
+        public void map(Object key, Text value, Context context)
+            throws IOException, InterruptedException
+        {
+            // Convert the Text object for the value to a String.
+            String line = value.toString();
+
+            int user_id = Integer.valueOf(line.split(",")[0]);
+
+            // If there's a semicolon, then they have friend values
+            if (line.contains(";")) {
+                // split semicolon, take second half, split comma to an array
+                String[] friend_list = line.split(";")[1].split(",");
+
+                context.write(new IntWritable(user_id),
+                    new IntWritable(friend_list.length));
+
+            }
+
+        }
+    }
+
+
+    public static class MyReducer1 extends
+        Reducer<IntWritable, IntWritable, IntWritable, IntWritable>
+    {
+        public void reduce(IntWritable key, Iterable<IntWritable> values,
+            Context context) throws IOException, InterruptedException
+        {
+            // Total the list of values associated with the address.
+            int count = 0;
+            for (IntWritable val : values) {
+                count += val.get();
+            }
+
+            context.write(key, new IntWritable(count));
+        }
+    }
+
+    /*
+     * This mapper finds the domain with the most users
+     */
+    public static class MyMapper2 extends
+        Mapper<Object, Text, Text, Text>
+    {
+        public void map(Object key, Text value, Context context)
+            throws IOException, InterruptedException
+        {
+            // Convert the Text object for the value to a string
+            String line = value.toString();
+
+            // write to context: constant, (domain, num users)
+            context.write(new Text("domain sum"), new Text(line));
+        }
+    }
+
+
+    public static class MyReducer2 extends
+        Reducer<Text, Text, Text, LongWritable>
+    {
+        public void reduce(Text key, Iterable<Text> values,
+            Context context) throws IOException, InterruptedException
+        {
+            String domain_max = "";
+            long num_users_max = 0;
+
+            for (Text val_text : values) {
+                String val = val_text.toString();
+                String[] val_split = val.split("\t");
+                String domain = val_split[0];
+                long num_users = Long.valueOf(val_split[1]);
+
+                if (num_users > num_users_max) {
+                    num_users_max = num_users;
+                    domain_max = domain;
+                }
+            }
+
+            context.write(new Text(domain_max),
+                new LongWritable(num_users_max));
+        }
+    }
 
     public static void main(String[] args) throws Exception {
-        Configuration conf = new Configuration();
-        Job job = Job.getInstance(conf, "problem 6");
-        job.setJarByClass(Problem6.class);
+        Configuration conf1 = new Configuration();
+        Job job1 = Job.getInstance(conf1, "problem 6");
+        job1.setJarByClass(Problem6.class);
 
         /* CHANGE THE CLASS NAMES AS NEEDED IN THE METHOD CALLS BELOW */
         // See Problem 4.java for comments describing the calls
 
-        job.setMapperClass(MyMapper.class);
-        job.setReducerClass(MyReducer.class);
+        job1.setMapperClass(MyMapper1.class);
+        job1.setReducerClass(MyReducer1.class);
 
-        job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(LongWritable.class);
-        //   job.setMapOutputKeyClass(Text.class);
-        job.setMapOutputValueClass(IntWritable.class);
+        job1.setOutputKeyClass(IntWritable.class);
+        job1.setOutputValueClass(IntWritable.class);
+        job1.setMapOutputKeyClass(IntWritable.class);
+        job1.setMapOutputValueClass(IntWritable.class);
 
-        job.setInputFormatClass(TextInputFormat.class);
-        FileInputFormat.addInputPath(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        job1.setInputFormatClass(TextInputFormat.class);
+        FileInputFormat.addInputPath(job1, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job1, new Path(args[1]));
 
-        job.waitForCompletion(true);
+        job1.waitForCompletion(true);
+
+        Configuration conf2 = new Configuration();
+        Job job2 = Job.getInstance(conf2, "problem 6");
+        job2.setJarByClass(Problem6.class);
+
+        /* CHANGE THE CLASS NAMES AS NEEDED IN THE METHOD CALLS BELOW */
+        // See Problem 4.java for comments describing the calls
+
+        job2.setMapperClass(MyMapper2.class);
+        job2.setReducerClass(MyReducer2.class);
+
+        job2.setOutputKeyClass(Text.class);
+        job2.setOutputValueClass(LongWritable.class);
+        //   job2.setMapOutputKeyClass(Text.class);
+        job2.setMapOutputValueClass(IntWritable.class);
+
+        job2.setInputFormatClass(TextInputFormat.class);
+        FileInputFormat.addInputPath(job2, new Path(args[1]));
+        FileOutputFormat.setOutputPath(job2, new Path(args[2]));
+
+        job2.waitForCompletion(true);
     }
 }
